b18ab1813cfbd35ff9fdee922f518ae7afde3d99
kasp4770 <kasp4770@edu.easj.dk>
2017-09-21 13:10:01 +0200

    inklusiv dokumentation

--- src/ArealCirkel.java
+++ src/ArealCirkel.java
@@ -8,14 +8,14 @@ public class ArealCirkel {
             System.out.println("Enter a number for Radius:   ");
 
             //de næste tre linjer "scanner" for brugerens indtastning
-            Scanner input = new Scanner(System.in);
-            double r = 0;
-            r = input.nextDouble();
+            Scanner input = new Scanner(System.in); //Brugeren kan indtaste sit tal
+            double r = 0; //radius er 0 eller større
+            r = input.nextDouble(); //Brugeren kan indputte sin radius
 
-            System.out.println("Radius er " + r);
+            System.out.println("Radius er " + r); //udprint af "Raduis er" og efterfølgende brugerens radius
 
-            double areal = 0;
-            areal = r*r*3.14159;
-            System.out.println("Arealet af cirklen er: "+ areal);
+            double areal = 0; //arealet er 0 eller større
+            areal = r*r*3.14159; //udregning af cirklens areal
+            System.out.println("Arealet af cirklen er: "+ areal); //udprint af tekst og areal
         }
     }
